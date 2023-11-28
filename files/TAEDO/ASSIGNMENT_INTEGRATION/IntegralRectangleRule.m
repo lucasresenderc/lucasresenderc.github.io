4326506ea105df8a11cddc8240ac1868bc59d9b0
lucasresenderc <lucasresenderc@gmail.com>
2023-11-28 03:51:19 -0300

    Recreate website with my own code.

--- files/TAEDO/ASSIGNMENT_INTEGRATION/IntegralRectangleRule.m
+++
@@ -1,8 +0,0 @@
-function integral = IntegralRectangleRule(times,curve,F)
-  N = size(times);
-  integral=[curve(1,:)*0];
-  
-  for i = 2:N(2)
-    integral(i,:)= integral(i-1,:) + F(times(i),curve(i,:))*(times(i)-times(i-1));
-  endfor
-endfunction
